b631f481462e7d3e7ecc70c961f1d07d61e085ee
Stephen Lewis <stephen@experiencehq.net>
2013-06-28 09:08:42 +0100

    Add bare-bones plugin file, and associated test file.

---
+++ src/smartypants/SmartyPantsPlugin.php
@@ -0,0 +1,25 @@
+<?php
+namespace Craft;
+
+/**
+ * SmartyPants Craft Twig filter.
+ *
+ * @author  Stephen Lewis <https://github.com/experience>
+ * @package SmartyPants
+ * @version 0.1.0
+ */
+
+class SmartyPantsPlugin extends BasePlugin {
+
+  /**
+   * Returns the plugin name.
+   *
+   * @return string
+   */
+  public function getName()
+  {
+
+  }
+
+}
+
---
+++ tests/SmartyPantsPluginTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Unit tests for the SmartyPants Craft Twig filter.
+ *
+ * @author  Stephen Lewis <https://github.com/experience>
+ * @package SmartyPants
+ */
+
+require_once __DIR__ .'/../src/smartypants/SmartyPantsPlugin.php';
+
+class SmartyPantsPluginTest extends PHPUnit_Framework_TestCase {
+
+  public function testThatThisIsRunning()
+  {
+    $this->assertTrue(true);
+  }
+
+}
+
